6d67a6f37bd613bd26fcfb4ae96a90f67d7a68c0
molk <40917695+molk15@users.noreply.github.com>
2023-05-19 14:38:57 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+//routes
+app.get('/',(req,res)=> {
+    res.render("index.ejs")
+} )
+app.get('/login',(req,res)=> {
+    res.render("login.ejs")
+} )
+app.get('/register',(req,res)=> {
+    res.render("register.ejs")
+} )
+//end routes
+app.listen(3000)
