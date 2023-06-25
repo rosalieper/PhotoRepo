b0332f850e30c7c99d3f6e0d9c3b43eb8bafd139
Perside Rosalie <perside.rosalie@wikimedia.de>
2023-06-25 02:52:58 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,10 +1,8 @@
 variable "access_key" {
-    default = "AKIAWFBWHWMEDQHA56NF"
     sensitive = true
 }
 
 variable "secret_key" {
-    default = "mfE233u36MdhAKkPyiJO4V13Hl8uIvhZH9Jvx7em"
     sensitive = true
 }
 
